5c7c564e351edd478498a52dad7ccdca1aaad3c8
Joaquin Montesano <montesano1994@gmail.com>
2026-02-02 11:19:44 -0300

    does it fix?

--- src/lib/aux.ts
+++ src/lib/aux.ts
@@ -1,12 +1,13 @@
 export function generateChunks(values: any) {
 	const chunks = [];
 	if (values && values.length > 0) {
-		for (let i = 0; i < values.length; i += 15) {
-			const chunk = values.slice(i, i + 15); // Fixed range to slice
-			if (chunk.length === 15) {
-				// Check if the chunk has 15 items
-				chunks.push(chunk);
-			}
+		// Find the first occurrence of a non-empty row to start chunking
+		let startIndex = values.findIndex((row: any[]) => row.length > 0);
+		if (startIndex === -1) return [];
+
+		for (let i = startIndex; i < values.length; i += 15) {
+			const chunk = values.slice(i, i + 15);
+			chunks.push(chunk);
 		}
 	}
 	return chunks;
